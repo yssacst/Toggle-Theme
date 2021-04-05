76f44758ffd24b6f82d950259238f64da935246c
Patrick Araujo <rayssacoosta@gmail.com>
2021-04-05 16:46:15 -0300

    toggle theme simple in React with variables css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+
+  const [darkTheme,setDarkTheme] = React.useState(true);
+  
+  const toggleTheme = ()=>{
+      setDarkTheme(!darkTheme) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={`App-header ${darkTheme ? `` : `light-mode`}`}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={toggleTheme}>Light/Dark MODE</button>
       </header>
     </div>
   );
